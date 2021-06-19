03fad6944a67a2aca0c2de2d699d44d191186c19
Lakhwinder Singh <lakhwinder.virk4@gmail.com>
2021-06-19 06:03:05 -0500

    Intital project setup

---
+++ AbstractTableView/AbstractTableView.h
@@ -0,0 +1,18 @@
+//
+//  AbstractTableView.h
+//  AbstractTableView
+//
+//  Created by Lakhwinder Singh on 2021-06-19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AbstractTableView.
+FOUNDATION_EXPORT double AbstractTableViewVersionNumber;
+
+//! Project version string for AbstractTableView.
+FOUNDATION_EXPORT const unsigned char AbstractTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbstractTableView/PublicHeader.h>
+
+
